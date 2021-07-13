0ffeeb5c02351cc6ce9d5264e227c6a28df044ac
chenxin <546507668mao>
2021-07-13 11:21:42 +0800

    react-demo

--- src/index.js
+++ src/index.js
@@ -1,17 +1,731 @@
+/*
+ * @Author: chen-xin
+ * @Date: 2021-07-06 14:39:29
+ * @LastEditTime: 2021-07-12 16:04:57
+ * @LastEditors: chen-xin
+ * @FilePath: \my-react-app\src\index.js
+ */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import {ThemeContext, themes} from "./theme-context";
+import ThemedButton from "./themed-button";
+import ThemeToggleButton from "./theme-toggle-button";
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [
+        {
+          squares: Array(9).fill(null)
+        }
+      ],
+      stepNumber: 0,
+      xIsNext: true
+    };
+  }
+
+  handleClick(i) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({
+      history: history.concat([
+        {
+          squares: squares
+        }
+      ]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0
+    });
+  }
+
+  render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+    }
+
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board
+            squares={current.squares}
+            onClick={i => this.handleClick(i)}
+          />
+        </div>
+        <div className="game-info">
+          <div>{status}</div>
+          <ol>{moves}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date()
+    }
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => {
+      this.tick()
+    }, 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID)
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>hell0</h1>
+        <h1>it is {this.state.date.toLocaleTimeString()}</h1>
+      </div>
+    )
+  }
+}
+
+function App() {
+  return (
+    <div>
+      <Clock/>
+      <Clock/>
+      <Clock/>
+    </div>
+  )
+}
+
+function UserGreeting(props) {
+  return <h1>welcome</h1>
+}
+
+function GuestGreeting(props) {
+  return <h1>please sign</h1>
+}
+
+function Greeting(props) {
+  const isLoggedIn = props.isLoggedIn;
+  if (isLoggedIn) {
+    return <UserGreeting/>
+  }
+  console.log(1)
+  return <GuestGreeting/>
+}
+
+function LoginButton(props) {
+  return (
+    <button onClick={props.onClick}>Login</button>
+  )
+}
+
+function LogoutButton(props) {
+  return (
+    <button onClick={props.onClick}>Logout</button>
+  )
+}
+
+class LoginControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+    this.handleLogoutClick = this.handleLogoutClick.bind(this);
+    this.state = {
+      isLoggedIn: false
+    }
+  }
+
+  handleLoginClick() {
+    this.setState({isLoggedIn: true})
+  }
+
+  handleLogoutClick() {
+    this.setState({isLoggedIn: false})
+  }
+
+  render() {
+    const isLoggedIn = this.state.isLoggedIn
+    let button;
+    if (isLoggedIn) {
+      button = <LogoutButton onClick={this.handleLogoutClick}/>;
+    } else {
+      button = <LoginButton onClick={this.handleLoginClick}/>;
+    }
+
+    return (
+      <div>
+        <Greeting isLoggedIn={isLoggedIn}/>
+        {button}
+      </div>
+    )
+  }
+}
+
+function MailBox(props) {
+  const message = props.message
+  return (
+    <div>
+      <h1>hello world!</h1>
+      {message.length > 0 &&
+      <h2>you have {message.length} message</h2>
+      }
+    </div>
+  )
+}
+
+const messages = ['React', 'Re: React', 'Re:Re: React'];
+
+function WarningBanner(props) {
+  if (!props.warn) {
+    return null;
+  }
+
+  return (
+    <div className="warning">
+      Warning!
+    </div>
+  );
+}
+
+class Page extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {showWarning: true};
+    this.handleToggleClick = this.handleToggleClick.bind(this);
+  }
+
+  handleToggleClick() {
+    this.setState(state => ({
+      showWarning: !state.showWarning
+    }));
+  }
+
+  render() {
+    return (
+      <div>
+        <WarningBanner warn={this.state.showWarning}/>
+        <button onClick={this.handleToggleClick}>
+          {this.state.showWarning ? 'Hide' : 'Show'}
+        </button>
+      </div>
+    );
+  }
+}
+
+const numbers = [1, 2, 3, 4, 5];
+
+function NumberList(props) {
+  const numbers = props.numbers;
+  const listItems = numbers.map((number) =>
+    <li key={number.toString()}>{number}</li>
+  )
+  return (
+    <ul>{listItems}</ul>
+  )
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+function BoilingVerdict(props) {
+  if (props.celsius >= 100) {
+    return <p>The water would boil.</p>
+  }
+  return <p>The water would not boil.</p>
+}
+
+const scaleNames = {
+  c: 'Celsius',
+  f: 'Fahrenheit'
+}
+
+class TemperatureInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this)
+    // this.state = {temperature: ''}
+  }
+
+  handleChange(e) {
+    // this.setState({
+    //   temperature: e.target.value
+    // })
+    this.props.onTemperatureChange(e.target.value)
+  }
+
+  render() {
+    // const temperature = this.state.temperature;
+    const temperature = this.props.temperature;
+    const scale = this.props.scale;
+    return (
+      <fieldset>
+        <legend>Enter temperature in {scaleNames[scale]}:</legend>
+        <input value={temperature} onChange={this.handleChange}/>
+        {/*<BoilingVerdict celsius={parseFloat(temperature)}/>*/}
+      </fieldset>
+    );
+  }
+}
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
+    this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
+    this.state = {temperature: '', scale: 'c'}
+  }
+
+  handleCelsiusChange(temperature) {
+    this.setState({scale: 'c', temperature})
+  }
+
+  handleFahrenheitChange(temperature) {
+    this.setState({scale: 'f', temperature})
+  }
+
+  render() {
+    const scale = this.state.scale;
+    const temperature = this.state.temperature;
+    const celsius = scale === 'f' ? tryConvert(temperature, toCelsius) : temperature;
+    const fahrenheit = scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature
+    return (
+      <div>
+        <TemperatureInput scale="c" temperature={celsius} onTemperatureChange={this.handleCelsiusChange}/>
+        <TemperatureInput scale="f" temperature={fahrenheit} onTemperatureChange={this.handleFahrenheitChange}/>
+        <BoilingVerdict celsius={parseFloat(celsius)}/>
+      </div>
+    )
+  }
+}
+
+function toCelsius(fahrenheit) {
+  return (fahrenheit - 32) * 5 / 9;
+}
+
+function toFahrenheit(celsius) {
+  return (celsius * 9 / 5) + 32;
+}
+
+function tryConvert(temperature, convert) {
+  const input = parseFloat(temperature);
+  if (Number.isNaN(input)) {
+    return '';
+  }
+  const output = convert(input);
+  const rounded = Math.round(output * 1000) / 1000;
+  return rounded.toString();
+}
+
+// ========================================
+
+const PRODUCTS = [
+  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+];
+
+// 展示类别
+class ProductCateGoryRow extends React.Component {
+  render() {
+    const category = this.props.category
+    return (
+      <tr>
+        <th colSpan='2'>
+          {category}
+        </th>
+      </tr>
+    )
+  }
+}
+
+// 展示 row
+class ProductRow extends React.Component {
+  render() {
+    const product = this.props.product;
+    const name = product.stocked ? product.name :
+      <span style={{color: 'red'}}>{product.name}</span>
+
+    return (
+      <tr>
+        <td>{name}</td>
+        <td>{product.price}</td>
+      </tr>
+    )
+  }
+}
+
+class ProductTable extends React.Component {
+  render() {
+    const filterText = this.props.filterText;
+    const inStockOnly = this.props.inStockOnly;
+
+    const rows = [];
+    let lastCategory = null;
+
+    this.props.products.forEach((product) => {
+      if (product.name.indexOf(filterText) === -1) {
+        return;
+      }
+
+      if (inStockOnly && !product.stocked) {
+        return;
+      }
+
+      if (product.category !== lastCategory) {
+        rows.push(
+          <ProductCateGoryRow
+            category={product.category}
+            key={product.category}
+          />
+        )
+      }
+      rows.push(
+        <ProductRow
+          product={product}
+          key={product.name}
+        />
+      )
+      lastCategory = product.category;
+    })
+    return (
+      <table>
+        <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+    )
+  }
+}
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+    this.handleInStockChange = this.handleInStockChange.bind(this);
+  }
+
+  handleFilterTextChange(e) {
+    this.props.onFilterTextChange(e.target.value);
+  }
+
+  handleInStockChange(e) {
+    this.props.onInStockChange(e.target.checked);
+  }
+
+  render() {
+    // const filterText = this.props.filterText;
+    // const inStockOnly = this.props.inStockOnly;
+    return (
+      <form>
+        <input
+          type='text'
+          placeholder='search...'
+          value={this.props.filterText}
+          onChange={this.handleFilterTextChange}
+        />
+        <p>
+          <input
+            type="checkbox"
+            checked={this.props.inStockOnly}
+            onChange={this.handleInStockChange}
+          />
+          {''}
+          Only show products in stock
+        </p>
+      </form>
+    )
+  }
+}
+
+class FilterableProductTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterText: '',
+      inStockOnly: false
+    }
+    this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+    this.handleInStockChange = this.handleInStockChange.bind(this);
+  }
+
+  handleFilterTextChange(filterText) {
+    this.setState({
+      filterText: filterText
+    })
+  }
+
+  handleInStockChange(inStockOnly) {
+    this.setState({
+      inStockOnly: inStockOnly
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar
+          filterText={this.state.filterText}
+          inStockOnly={this.state.inStockOnly}
+          onFilterTextChange={this.handleFilterTextChange}
+          onInStockChange={this.handleInStockChange}
+        />
+        <ProductTable
+          products={this.props.products}
+          filterText={this.state.filterText}
+          inStockOnly={this.state.inStockOnly}
+        />
+      </div>
+    )
+  }
+}
+
+// =======================
+class Section extends React.Component {
+  render() {
+    return null;
+  }
+}
+
+// const ThemeContext = React.createContext('light');
+class NewApp extends React.Component {
+  constructor(props) {
+    super(props);
+    // this.state = {
+    //   theme: themes.light
+    // }
+
+    this.toggleTheme = () => {
+      this.setState(state => ({
+        theme: state.theme === themes.dark ? themes.light : themes.dark
+      }))
+    }
+
+    this.state = {
+      theme: themes.light,
+      toggleTheme: this.toggleTheme
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <ThemeContext.Provider value={this.state}>
+          <Content />
+        </ThemeContext.Provider>
+        <Section>
+          {/*<ThemedButton/>*/}
+        </Section>
+      </div>
+    )
+  }
+}
+function Content() {
+  return (
+    <div>
+      <ThemeToggleButton />
+    </div>
+  )
+}
+
+function ToolBar(props) {
+  return (
+    <ThemedButton onClick={props.changeTheme}>
+      change Theme
+    </ThemedButton>
+  )
+}
+
+class ThemeButton extends React.Component {
+  static contextType = ThemeContext;
+
+  render() {
+    console.log('this.context:', this.context)
+    return (
+      <span>{this.context}</span>
+    )
+  }
+}
+
+function FancyButton(props) {
+  return (
+    <button className="FancyButton">
+      {props.children}
+    </button>
+  )
+}
+
+class Table extends React.Component {
+  render() {
+    return (
+      <table>
+        <tr>
+          <Columns></Columns>
+        </tr>
+      </table>
+    )
+  }
+}
+
+class Columns extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <td>hello</td>
+        <td>world</td>
+      </React.Fragment> 
+    )
+  }
+}
+
+function withSubscription(WrappedComponent, selectData) {
+  return class extends React.Component {
+    constructor(props) {
+      super(props);
+      this.handleChange = this.handleChange.bind(this);
+      this.state = {
+        data: selectData(DataSource, props)
+      }
+    }
+  
+    componentDidMount() {
+      DataSource.addChangeListener(this.handleChange);
+    }
+   
+    componentWillUnmount() {
+      DataSource.removeChangeListener(this.handleChange);
+    }
+  
+    handleChange() {
+      this.setState({
+        data: selectData(DataSource, this.props)
+      })
+    }
+  
+    render() {
+      return <WrappedComponent data={this.state.data} {...this.props} />
+    }
+  }
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  // <NewApp/>,
+  // <Game/>,
+  // <Greeting isLoggedIn={false}/>,
+  // <LoginControl/>,
+  // <MailBox message={messages}/>,
+  // <Page/>,
+  // <NumberList numbers={numbers}/>,
+  // <Calculator/>,
+  // <FilterableProductTable products={PRODUCTS}/>,
+  // <FancyButton children='xxx'/>,
+  <Table />,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
